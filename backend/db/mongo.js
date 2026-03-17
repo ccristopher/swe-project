8103c25445c1537a41d6e5206727c49a8ac7c74d
Rawanh04 <rawanahussain04@gmail.com>
2026-03-16 21:10:37 -0400

    add the required npm, connect the databse to code and verification file

---
+++ backend/db/mongo.js
@@ -0,0 +1,34 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pfigh3v.mongodb.net/pet_prose?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    console.log("Connected to MongoDB!");
+    await client.db("pet_n_prose").command({ ping: 1 });
+    console.log("Pinged your deployment successfully!");
+
+    const db = client.db("pet_n_prose");
+    const collections = await db.listCollections().toArray();
+    console.log("Collections:", collections);
+
+  } catch (err) {
+    console.error("Connection failed:", err);
+  } finally {
+    await client.close();
+  }
+}
+
+run();
